8e9f7d3a39e90f727fb51f99144e88bdd835adce
Parul Koul <pkoul@wellesley.edu>
2018-01-27 21:44:27 -0500

    Changing the index page

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,18 @@
 function main() {
   get('/api/whoami', {}, function(user) {
     renderNavbar(user);
+
+    const descriptionDiv = document.getElementById('description');
+    descriptionDiv.innerHTML = "This is a text based role playing game about navigating Wellesley College.";
+
+    const button = document.getElementById('play-button');
+
+    button.addEventListener ("click", function() {
+        itemLink = document.createElement('a');
+      itemLink.href = "/u/game";
+      return itemLink;
+    });
+
     // renderStories(user);
     //
     // const socket = io();
